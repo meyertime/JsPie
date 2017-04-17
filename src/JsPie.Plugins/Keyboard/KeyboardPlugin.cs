09634894e2542d1d299dd885a1556cf8ed73ce83
David Meyer <dave@deftflux.net>
2017-04-17 14:27:37 -0500

    Added more keys to keyboard plugin.

--- src/JsPie.Plugins/Keyboard/KeyboardPlugin.cs
+++ src/JsPie.Plugins/Keyboard/KeyboardPlugin.cs
@@ -7,49 +7,138 @@ namespace JsPie.Plugins.Keyboard
 {
     public class KeyboardPlugin : IInputPlugin, IOutputPlugin, IDisposable
     {
-        private static ControllerId ControllerId = new ControllerId("keyboard");
-        private static KeyboardControlInfo[] Controls = new[]
+        private static ControllerId ControllerId;
+        private static List<KeyboardControlInfo> Controls;
+
+        private static ControllerInfo ControllerInfo;
+
+        static KeyboardPlugin()
         {
-            Key("zero", 48),
-            Key("one", 49),
-            Key("two", 50),
-            Key("three", 51),
-            Key("four", 52),
-            Key("five", 53),
-            Key("six", 54),
-            Key("seven", 55),
-            Key("eight", 56),
-            Key("nine", 57),
+            ControllerId = new ControllerId("keyboard");
 
-            Key("a", 65),
-            Key("b", 66),
-            Key("c", 67),
-            Key("d", 68),
-            Key("e", 69),
-            Key("f", 70),
-            Key("g", 71),
-            Key("h", 72),
-            Key("i", 73),
-            Key("j", 74),
-            Key("k", 75),
-            Key("l", 76),
-            Key("m", 77),
-            Key("n", 78),
-            Key("o", 79),
-            Key("p", 80),
-            Key("q", 81),
-            Key("r", 82),
-            Key("s", 83),
-            Key("t", 84),
-            Key("u", 85),
-            Key("v", 86),
-            Key("w", 87),
-            Key("x", 88),
-            Key("y", 89),
-            Key("z", 90)
-        };
+            Controls = new List<KeyboardControlInfo>
+            {
+                Key("backSpace", 8),
+                Key("tab",  9),
+                Key("enter", 13),
+                Key("shift", 16),
+                Key("ctrl", 17),
+                Key("alt", 18),
+                Key("pause", 19),
+                Key("capsLock", 20),
+                Key("esc", 27),
+                Key("space", 32),
+                Key("pageUp", 33),
+                Key("pageDown", 34),
+                Key("end", 35),
+                Key("home", 36),
+                Key("left", 37),
+                Key("up", 38),
+                Key("right", 39),
+                Key("down", 40),
+                Key("insert", 45),
+                Key("delete", 46),
 
-        private static ControllerInfo ControllerInfo = new ControllerInfo(ControllerId.Name, 1, "The keyboard.", Controls.Select(c => c.ControlInfo));
+                Key("zero", 48),
+                Key("one", 49),
+                Key("two", 50),
+                Key("three", 51),
+                Key("four", 52),
+                Key("five", 53),
+                Key("six", 54),
+                Key("seven", 55),
+                Key("eight", 56),
+                Key("nine", 57),
+
+                Key("a", 65),
+                Key("b", 66),
+                Key("c", 67),
+                Key("d", 68),
+                Key("e", 69),
+                Key("f", 70),
+                Key("g", 71),
+                Key("h", 72),
+                Key("i", 73),
+                Key("j", 74),
+                Key("k", 75),
+                Key("l", 76),
+                Key("m", 77),
+                Key("n", 78),
+                Key("o", 79),
+                Key("p", 80),
+                Key("q", 81),
+                Key("r", 82),
+                Key("s", 83),
+                Key("t", 84),
+                Key("u", 85),
+                Key("v", 86),
+                Key("w", 87),
+                Key("x", 88),
+                Key("y", 89),
+                Key("z", 90),
+
+                Key("lwin", 91),
+                Key("rwin", 92),
+                Key("select", 93),
+                Key("numpad0", 96),
+                Key("numpad1", 97),
+                Key("numpad2", 98),
+                Key("numpad3", 99),
+                Key("numpad4", 100),
+                Key("numpad5", 101),
+                Key("numpad6", 102),
+                Key("numpad7", 103),
+                Key("numpad8", 104),
+                Key("numpad9", 105),
+                Key("ultiply", 106),
+                Key("add", 107),
+                Key("subtract", 109),
+                Key("decimal", 110),
+                Key("divide", 111),
+                Key("f1", 112),
+                Key("f2", 113),
+                Key("f3", 114),
+                Key("f4", 115),
+                Key("f5", 116),
+                Key("f6", 117),
+                Key("f7", 118),
+                Key("f8", 119),
+                Key("f9", 120),
+                Key("f10", 121),
+                Key("f11", 122),
+                Key("f12", 123),
+                Key("numLock", 144),
+                Key("scrollLock", 145),
+                Key("lshift", 160),
+                Key("rshift", 161),
+                Key("lctrl", 162),
+                Key("rctrl", 163),
+                Key("lalt", 164),
+                Key("ralt", 165),
+                Key("semicolon", 186),
+                Key("equals", 187),
+                Key("comma", 188),
+                Key("dash", 189),
+                Key("period", 190),
+                Key("forwardSlash", 191),
+                Key("tilde", 192),
+                Key("openBracket", 219),
+                Key("backSlash", 220),
+                Key("closeBraket", 221),
+                Key("apostrophe", 222),
+                Key("fn", 255)
+            };
+
+            for (int i = 0; i <= 255; i++)
+            {
+                if (!Controls.Any(c => c.KeyCode == i))
+                {
+                    Controls.Add(Key($"keycode${i}", i));
+                }
+            }
+
+            ControllerInfo = new ControllerInfo(ControllerId.Name, 1, "The keyboard.", Controls.Select(c => c.ControlInfo));
+        }
 
         private static KeyboardControlInfo Key(string name, int keyCode, string description = null)
         {
