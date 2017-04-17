56cc02af5a13f396ef4f08acc0e3606df293edac
David Meyer <dave@deftflux.net>
2017-04-17 14:29:23 -0500

    Ps3ControlSet stores control state and should not be static.

--- src/JsPie.Plugins/Ps3/Ps3Plugin.cs
+++ src/JsPie.Plugins/Ps3/Ps3Plugin.cs
@@ -9,8 +9,9 @@ namespace JsPie.Plugins.Ps3
     public class Ps3Plugin : IInputPlugin, IDisposable
     {
         private static readonly ControllerId ControllerId = new ControllerId("ps3");
-        private static readonly Ps3ControlSet ControlSet = new Ps3ControlSet(ControllerId);
-        private static ControllerInfo ControllerInfo = new ControllerInfo(ControllerId.Name, 1, "PlayStation3 controller.", ControlSet.Controls.Select(c => c.Ps3ControlInfo.ControlInfo));
+
+        private readonly Ps3ControlSet _controlSet;
+        private readonly ControllerInfo _controllerInfo;
 
         private readonly Ps3UsbDeviceInterface _device;
         private readonly Task _pollTask;
@@ -19,6 +20,9 @@ namespace JsPie.Plugins.Ps3
         
         public Ps3Plugin()
         {
+            _controlSet = new Ps3ControlSet(ControllerId);
+            _controllerInfo = new ControllerInfo(ControllerId.Name, 1, "PlayStation3 controller.", _controlSet.Controls.Select(c => c.Ps3ControlInfo.ControlInfo));
+            
             // TODO: Multiple controllers and detect when controllers are connected / disconnected
             var enumerator = new Ps3UsbDeviceEnumerator();
             var devices = enumerator.GetDevices();
@@ -47,7 +51,7 @@ namespace JsPie.Plugins.Ps3
                 
         public IEnumerable<ControllerInfo> GetControllers()
         {
-            yield return ControllerInfo;
+            yield return _controllerInfo;
         }
 
         private void Poll()
@@ -61,7 +65,7 @@ namespace JsPie.Plugins.Ps3
 
                 var events = (List<ControlEvent>)null;
 
-                foreach (var control in ControlSet.Controls)
+                foreach (var control in _controlSet.Controls)
                 {
                     var @event = control.UpdateValue(data);
                     if (@event != null)
