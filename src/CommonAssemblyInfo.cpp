84f1701d17628793e8cc0885d07af981ae7fce57
David Meyer <dave@deftflux.net>
2017-04-17 22:58:38 -0500

    Version 0.0.0.0

---
+++ src/CommonAssemblyInfo.cpp
@@ -0,0 +1,11 @@
+﻿using namespace System::Reflection;
+
+[assembly:AssemblyConfiguration(L"")];
+[assembly:AssemblyCompany(L"")];
+[assembly:AssemblyProduct(L"JsPie")];
+[assembly:AssemblyCopyright(L"Copyright © 2016-2017")];
+[assembly:AssemblyTrademark(L"")];
+[assembly:AssemblyCulture(L"")];
+
+[assembly:AssemblyVersion("0.0.0.0")];
+[assembly:AssemblyFileVersion("0.0.0.0")];
---
+++ src/CommonAssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("JsPie")]
+[assembly: AssemblyCopyright("Copyright © 2016-2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
